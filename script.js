ad865a90975d2ba40be1dc9c1dd505165446bb29
WillDSM <111542656+WillDSM@users.noreply.github.com>
2023-06-13 22:09:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -72,4 +72,14 @@ function listar(vet){
         res.appendChild(p);
         }
     }
+    
+    function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '0px';
+  }
+}
+
 }
